650ec7c1d76cef5cd028db7309254a2f080ad466
Serumola <pakoserumola02@gmail.com>
2026-05-05 15:46:26 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import { StrictMode } from 'react';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<StrictMode>
-    <App />
-</StrictMode> 
-
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
